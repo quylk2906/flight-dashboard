bc544f9ef1d07c2dfda74fc61296ffec4f6ccbc9
Quylk <quy.le@beesightsoft>
2018-07-06 19:24:05 +0700

    Add email

--- src/app/theme/pages/default/components/agency/agency.module.ts
+++ src/app/theme/pages/default/components/agency/agency.module.ts
@@ -1,21 +1,21 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { Routes, RouterModule } from '@angular/router';
-import { AgencyComponent } from './agency.component';
-import { LayoutModule } from '../../../../layouts/layout.module';
-import { DefaultComponent } from '../../../default/default.component';
-import { FormsModule } from '@angular/forms';
-import { AgencyService } from '../../../../../_services/agency.service';
-import { DataTablesModule } from 'angular-datatables';
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { Routes, RouterModule } from "@angular/router";
+import { AgencyComponent } from "./agency.component";
+import { LayoutModule } from "../../../../layouts/layout.module";
+import { DefaultComponent } from "../../../default/default.component";
+import { FormsModule } from "@angular/forms";
+import { AgencyService } from "../../../../../_services/agency.service";
+import { DataTablesModule } from "angular-datatables";
 
 const routes: Routes = [
   {
-    "path": "",
-    "component": DefaultComponent,
-    "children": [
+    path: "",
+    component: DefaultComponent,
+    children: [
       {
-        "path": "",
-        "component": AgencyComponent
+        path: "",
+        component: AgencyComponent
       }
     ]
   }
@@ -23,15 +23,14 @@ const routes: Routes = [
 
 @NgModule({
   imports: [
-    CommonModule, RouterModule.forChild(routes), LayoutModule, FormsModule, DataTablesModule
-  ], exports: [
-    RouterModule
+    CommonModule,
+    RouterModule.forChild(routes),
+    LayoutModule,
+    FormsModule,
+    DataTablesModule
   ],
-  providers: [
-    AgencyService
-  ],
-  declarations: [
-    AgencyComponent
-  ]
+  exports: [RouterModule],
+  providers: [AgencyService],
+  declarations: [AgencyComponent]
 })
-export class AgencyModule { }
+export class AgencyModule {}
--- src/app/theme/pages/default/components/approve/approve.component.ts
+++ src/app/theme/pages/default/components/approve/approve.component.ts
@@ -1,36 +1,38 @@
-import { Component, OnInit, AfterViewInit, ViewEncapsulation, OnDestroy } from '@angular/core';
-import { ScriptLoaderService } from '../../../../../_services/script-loader.service';
-import { NgForm } from '@angular/forms';
-import { Subscription } from 'rxjs/Subscription';
-import { trimObjectAfterSave } from '../../../../../_utils/trimObject';
-import { find } from 'lodash';
-import { ObjectUnsubscribedError, Subject } from 'rxjs';
-import { ToastrService } from 'ngx-toastr';
-import { DataTableDirective } from 'angular-datatables';
-import { ViewChild } from '@angular/core';
-import { Helpers } from '../../../../../helpers';
-import { ClientTicket } from '../../../../../_models/client-ticket.model';
-import { ClientTicketService } from '../../../../../_services/client-ticket.service';
-
-
+import {
+  Component,
+  OnInit,
+  AfterViewInit,
+  ViewEncapsulation,
+  OnDestroy
+} from "@angular/core";
+import { ScriptLoaderService } from "../../../../../_services/script-loader.service";
+import { NgForm } from "@angular/forms";
+import { Subscription } from "rxjs/Subscription";
+import { trimObjectAfterSave } from "../../../../../_utils/trimObject";
+import { find } from "lodash";
+import { ObjectUnsubscribedError, Subject } from "rxjs";
+import { ToastrService } from "ngx-toastr";
+import { DataTableDirective } from "angular-datatables";
+import { ViewChild } from "@angular/core";
+import { Helpers } from "../../../../../helpers";
+import { ClientTicket } from "../../../../../_models/client-ticket.model";
+import { ClientTicketService } from "../../../../../_services/client-ticket.service";
 
 @Component({
-  selector: 'app-approve',
-  templateUrl: './approve.component.html',
+  selector: "app-approve",
+  templateUrl: "./approve.component.html",
   encapsulation: ViewEncapsulation.None,
   styles: []
 })
 export class ApproveComponent implements OnInit, OnDestroy, AfterViewInit {
-  private subsArr: Subscription[]
-  list: ClientTicket[]
-  @ViewChild(DataTableDirective)
-  dtElement: DataTableDirective;
-  private listStatus: string[] = ['None', 'In Process', 'Approved', 'Rejected']
+  private subsArr: Subscription[];
+  list: ClientTicket[];
+  @ViewChild(DataTableDirective) dtElement: DataTableDirective;
+  private listStatus: string[] = ["None", "In Process", "Approved", "Rejected"];
   dtOptions: DataTables.Settings = {
     responsive: true,
     pagingType: "full_numbers",
-    columnDefs: [
-    ],
+    columnDefs: [],
     order: [[0, "desc"]]
   };
   dtTrigger = new Subject();
@@ -54,67 +56,51 @@ export class ApproveComponent implements OnInit, OnDestroy, AfterViewInit {
     _id: undefined,
     createdAt: undefined,
     updatedAt: undefined
-  }
+  };
 
-  constructor(private _script: ScriptLoaderService,
+  constructor(
+    private _script: ScriptLoaderService,
     private _serviceClient: ClientTicketService,
     private _toastr: ToastrService
-  ) {
-  }
-
+  ) {}
 
   ngOnInit() {
-    this.subsArr = []
-    Helpers.setLoading(true)
+    this.subsArr = [];
+    Helpers.setLoading(true);
 
-    this.list = this._serviceClient.getClients()
-    this._serviceClient.loadData()
-
+    this.list = this._serviceClient.getClients();
+    this._serviceClient.loadData();
 
     const sub1 = this._serviceClient.listClientTicketsChanged.subscribe(
       rs => {
-        this.list = rs
+        this.list = rs;
         console.log(rs);
-        this.rerender()
+        this.rerender();
       },
       err => {
         console.log(err);
-      })
-
+      }
+    );
   }
-
-  // onDelete(id) {
-  //   Helpers.setLoading(true)
-  //   const sub = this._serviceClient.deleteDeposit(id).subscribe(rs => {
-  //     if (rs['count'] !== 0) {
-  //       this._toastr.info('Xóa thành công', undefined, { closeButton: true });
-  //       this._serviceClient.loadData()
-  //     }
-  //   })
-  //   this.subsArr.push(sub)
-  // }
 
   onShowModal(id) {
-    this.currentItem = find(this.list, (item) => { return item._id == id })
+    this.currentItem = find(this.list, item => {
+      return item._id == id;
+    });
   }
-  // onEdit(id) {
-  //   this.currentItem = find(this.list, (item) => { return item.id == id })
-  //   $("#m_select2_4_1").val(this.currentItem.agencyId).trigger('change')
-  // }
 
   ngOnDestroy(): void {
-    Helpers.setLoading(true)
-    this.subsArr.forEach(sub => sub.unsubscribe())
+    Helpers.setLoading(true);
+    this.subsArr.forEach(sub => sub.unsubscribe());
     this.dtTrigger.unsubscribe();
   }
 
   ngAfterViewInit() {
-    this._script.loadScripts('app-approve',
-      [
-        'assets/vendors/custom/datatables/datatables.bundle.js',
-        'assets/demo/default/custom/crud/datatables/standard/paginations.js',
-        'assets/demo/default/custom/crud/forms/validation/form-controls.js'
-      ]);
+    this._script.loadScripts("app-approve", [
+      "assets/vendors/custom/datatables/datatables.bundle.js",
+      "assets/demo/default/custom/crud/datatables/standard/paginations.js",
+      "assets/demo/default/custom/crud/forms/validation/form-controls.js"
+    ]);
     this.dtTrigger.next();
   }
 
@@ -122,7 +108,6 @@ export class ApproveComponent implements OnInit, OnDestroy, AfterViewInit {
     // const dataAgency = this.listAgencies.map(item => {
     //   return { id: item.agencyCode, text: item.agencyName }
     // })
-
     // $("#m_select2_4_1").select2({ data: dataAgency })
     // this._script.loadScripts('app-approve',
     //   [
@@ -130,21 +115,51 @@ export class ApproveComponent implements OnInit, OnDestroy, AfterViewInit {
     //   ]);
   }
 
+  // onSendEmail() {
+  //   const sub =
+  //   this.subsArr.push(sub);
+  // }
+
   onApprove() {
-    this.currentItem.tinhTrangVe = this.listStatus[2]
-    console.log('onApprove', this.currentItem);
-    const sub = this._serviceClient.changeStatus(this.currentItem).subscribe(rs => {
-      console.log(rs);
-    })
-    this.subsArr.push(sub)
+    this.currentItem.tinhTrangVe = this.listStatus[2];
+    const email = "quylk2906@gmail.com";
+    let data = { ...this.currentItem } as any;
+    data._user = "current_user";
+    data.email = email;
+    const sub = this._serviceClient
+      .changeStatus(this.currentItem)
+      .subscribe(rs => {
+        return this._serviceClient.sendEmail(data).subscribe(
+          rs => {
+            console.log(rs);
+          },
+          err => {
+            console.log(err);
+          }
+        );
+      });
+    this.subsArr.push(sub);
   }
 
   onReject() {
-    this.currentItem.tinhTrangVe = this.listStatus[3]
-    const sub = this._serviceClient.changeStatus(this.currentItem).subscribe(rs => {
-      console.log(rs);
-    })
-    this.subsArr.push(sub)
+    this.currentItem.tinhTrangVe = this.listStatus[3];
+    const email = "quylk2906@gmail.com";
+    let data = { ...this.currentItem } as any;
+    data._user = "current_user";
+    data.email = email;
+    const sub = this._serviceClient
+      .changeStatus(this.currentItem)
+      .subscribe(rs => {
+        return this._serviceClient.sendEmail(data).subscribe(
+          rs1 => {
+            console.log(rs1);
+          },
+          err => {
+            console.log(err);
+          }
+        );
+      });
+    this.subsArr.push(sub);
   }
 
   rerender() {
@@ -153,7 +168,7 @@ export class ApproveComponent implements OnInit, OnDestroy, AfterViewInit {
       dtInstance.destroy();
       // Call the dtTrigger to rerender again
       this.dtTrigger.next();
-      Helpers.setLoading(false)
+      Helpers.setLoading(false);
     });
   }
 }
