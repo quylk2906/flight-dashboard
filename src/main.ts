dc0ab7c582ffa15e9e82c67060821f6f8c68718a
QUYHEO\quy.heo <quylk2906@gmail.com>
2018-09-27 15:05:32 +0700

    Angular Universal still not working.

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,37 @@
-import { enableProdMode } from '@angular/core';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { enableProdMode } from "@angular/core";
+import { platformBrowserDynamic } from "@angular/platform-browser-dynamic";
 
-import { AppModule } from './app/app.module';
-import { environment } from './environments/environment';
+import { AppModule } from "./app/app.module";
+import { environment } from "./environments/environment";
+import { Observable } from "rxjs";
+// import { Observable } from "rxjs";
+
+const debuggerOn = true;
+
+const debug = (message: string) => {
+  return this.tap(
+    nextVal => {
+      if (debuggerOn) {
+        console.log(message, nextVal);
+      }
+    },
+    error => {
+      if (debuggerOn) {
+        console.error(message, error);
+      }
+    },
+    () => {
+      if (debuggerOn) {
+        console.error("Observable completed - ", message);
+      }
+    }
+  );
+};
 
 if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
+platformBrowserDynamic()
+  .bootstrapModule(AppModule)
   .catch(err => console.log(err));
